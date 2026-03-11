64b3f67e0ac5889ec624b05889bc4cd119aa95ed
Nicholas Phang <23058np@hvhs.school.nz>
2026-03-11 22:44:35 +1300

    Slimmed down code, haveing a clear vision of end.

--- javascript/game.js
+++ javascript/game.js
@@ -1,108 +1,94 @@
-let fire
+
 function setup() {  
-    rgbR = 230;
-    rgbG = 230;
-    rgbB = 230;
-    gameStarted = false;
     textSize(24);
     fill(255);
     stroke("white");
+    textAlign(CENTER);
     canvas = createCanvas(500,500);
     startButton = new Sprite(width/2,height/2,100*1.61,100,'s');
-    console.log(startButton);
     startButton.color = ("white");
     startButton.text = ("Start Game");
     startButton.overlaps(allSprites);
-    stick = new Sprite(width/2,height/2,10,100,'s');
+    stick = new Sprite(width/2,height/2+25,10,100,'s');
     stick.color = ("rgb(77, 59, 32)");
     stick.strokeWeight = 0;
     stick.opacity = 0;
-
-    fire = new Group();
-    
+    stick.offset.y = -25; 
 }
+gameStarted = false;
+gameEnded = false;
 stickMovement = 0;
 totalMovement = 0;
 friction = 0;
 frictionIncrement = 0;
-fireStarted = false;
-frictionThreshold = 10;
-afv = 0;
-function draw() {
-    if (gameStarted == true) { 
-     
-        background(rgbR, rgbG, rgbB);
-        function getStickOrientation() {
-            left = Math.min(kb.pressing("left"),1);
-            right = Math.min(kb.pressing("right"),1);
-            stickSide = (left - right);
-        }
+frictionThreshold = 3;
 
-        function moveStick() {
-            if (stickSide != stickMovement) {
-                totalMovement = stickMovement - stickSide
-            }
-            return totalMovement
-            
-        }
-        totalMovement = 0; // why does it break if not here?
-        function generateFriction () {
-            frictionIncrement = Math.abs(totalMovement);
-            friction = friction + frictionIncrement;
-            text(friction, 50,50);
-            function checkThreshold() {
-                function startFire() {
-                    fireStarted = true;
-                }
-                if (friction >= frictionThreshold) {
-                    startFire();
-                }
-            }
-            checkThreshold();
-        }
-        
-        function stickVisuals() {
-        stick.opacity += 0.03;
-        stick.x = width/2-50*stickSide;
-        }
 
-        // Running functions seperater
-        
-        getStickOrientation();
-        moveStick(); 
-        if (fireStarted == true && afv == 0) {
-            endGame();
-            afv = 1;
-        }
-        generateFriction();
-        stickVisuals(); //Must be last to run
-        stickMovement = stickSide;// this has to go even laster.. idk
-    } else {
-        background("rgb(0, 0, 0)");
-        if (startButton.mouse.pressed()) {
-        startGame();
-        }
-    }
-}
-/*
-left to right
-it need to track when and how far you go left to right
-*/
 function startGame() {
     gameStarted = true;
     startButton.remove();
 }
-function endGame() {
-    console.log("gamedone");
-    for (i = 0; i < 100; i++) {
-    fire.diameter = random(10,20);
-    fire.drag = random(1,10)
-    fire.vel.x = random(-5,5)
-    fire.vel.y = random(1,5)
-    fire = new Sprite()
-    //https://www.youtube.com/watch?v=qBt_w3SELnw
-    } 
+function makeWinningEmberJelliesOfVictory() {
+    WinningEmberJellyOfVictory = new Sprite(width/2,height/2+25,10,'d');
+    WinningEmberJellyOfVictory.color = color(255,random(150,160),random(49,57));
+    WinningEmberJellyOfVictory.strokeWeight = 0;
+    WinningEmberJellyOfVictory.scale = random(1,2);
+    WinningEmberJellyOfVictory.life = random(100,200);
+    WinningEmberJellyOfVictory.drag.x = 0.001;
+    WinningEmberJellyOfVictory.vel.x += random(0.3,-0.3)
+    WinningEmberJellyOfVictory.vel.y -= random(0,0.2)
+    WinningEmberJellyOfVictory.opacity = 0.5;
+    WinningEmberJellyOfVictory.overlaps(allSprites);
+
 }
+function endGame() {
+    textSize(32);
+    if (gameEnded == false) {
+        seconds = nf(millis() / 1000,1,0)
+        
+        gameEnded = true;         
+    }
+    makeWinningEmberJelliesOfVictory();
+    
+    text("took " + seconds + "s to win",width/2, 450);
+    
+    
+    
+    
+}
+function draw() {
+    textSize(100);
+    if (gameStarted == true) { 
+        background(230,230,230);
+
+        left = Math.min(kb.pressing("left"),1);
+        right = Math.min(kb.pressing("right"),1);
+        stickSide = (left - right);
+        totalMovement = stickMovement - stickSide;
+        frictionIncrement = Math.abs(totalMovement);
+        // this need to be abosolute because totalMovement can be negative, 
+        // and we dont want the increment to go backwards.
+        friction = friction + frictionIncrement;
+        text(friction, width/2,100);
+        if (friction >= frictionThreshold) {
+            endGame()
+        }
+        stickMovement = stickSide;
+        totalMovement = 0; 
+
+        stick.opacity += 0.03;
+        stick.rotation = -15*stickSide;
+    } else {
+        background("rgb(0, 0, 0)");
+        if (startButton.mouse.pressed()) {
+            startGame();
+        }
+    }
+    
+}
+
+
+
 
 // variable startButton can be referred to  as t or testB or test in the code after  declaration if there are no other variables that shaare theese letters. use as little as possible
 
