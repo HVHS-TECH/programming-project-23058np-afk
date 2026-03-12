11b9e94bce52eb9d428d2629f3baa9496cfcec1d
nicholas <23058np@hvhs.school.nz>
2026-03-13 10:00:43 +1300

    broke it and cleaned it

--- javascript/game.js
+++ javascript/game.js
@@ -1,6 +1,18 @@
 
 /*Spamming game that requires you to spam left and right arrow keys as fast as you can, 
 With theme of lighting a fire*/
+// all setup vars here
+let gameStarted = false;
+let gameEnded = false;
+let stickMovement = 0
+let totalMovement = 0;
+let friction = 0;
+let controlText = `
+Spam left & right arrow to increase heat.\n
+Get to 50 heat to start a fire.\n
+Start a fire quick as possible!
+`;
+const FRICTION_THRESHOLD = 50;
 
 function setup() {  
     textSize(24);
@@ -8,78 +20,93 @@ function setup() {
     stroke("white");
     textAlign(CENTER); 
     canvas = createCanvas(500,500);
+    function beforeGame() { //variable scope defined?
+        function createStartButton() {
+            startButton = new Sprite(width/2,height/2,100*1.61,100,'s');
+            startButton.color = ("white");
+            startButton.text = ("Start Game");
+            startButton.overlaps(allSprites);
+        }
 
-    startButton = new Sprite(width/2,height/2,100*1.61,100,'s');
-    startButton.color = ("white");
-    startButton.text = ("Start Game");
-    startButton.overlaps(allSprites);
+        function createControlButton() {
+            controlButton = new Sprite(width/2,100,150,50,'s');
+            controlButton.color = ("white");
+            controlButton.text = ("Controls");
+        }
 
-    stick = new Sprite(width/2,height/2+25,10,100,'s');
-    stick.color = ("rgb(77, 59, 32)");
-    stick.strokeWeight = 0;
-    stick.visible = false;
-    stick.offset.y = -25; 
+        function createStick() {
+                stick = new Sprite(width/2,height/2+25,10,100,'s');
+                stick.color = ("rgb(77, 59, 32)");
+                stick.strokeWeight = 0;
+                stick.visible = false;
+                stick.offset.y = -45;
+        }
 
-    campfire = new Sprite(width/2,height/2+25,0,0,"n");
-    campfire.image = '../assets/images/campfire.jpg';
-    campfire.image.scale = 0.3;
-    campfire.layer = 0;
-    campfire.visible = false;
+        function createCamp() {
+            campfire = new Sprite(width/2,height/2+25,0,0,"n");
+            campfire.image = '../assets/images/campfire.jpg';
+            campfire.image.scale = 0.3;
+            campfire.layer = 0;
+            campfire.visible = false;
+        }
 
-    controlButton = new Sprite(width/2,100,150,50,'s');
-    controlButton.color = ("white");
-    controlButton.text = ("Controls");
+        createStick();
+        createCamp();
+        createStartButton();
+        createControlButton();
+    }
+    beforeGame();
 }
-
-let gameStarted = false;
-let gameEnded = false;
-let stickMovement = 0
-let totalMovement = 0;
-let friction = 0;
-const FRICTION_THRESHOLD = 50;
 
 function startGame() {
     gameStarted = true;
-    secondsToStart = millis() / 1000,1; // "starts" a timer
-    campfire.visible = true;
-    stick.visible = true;
-    startButton.remove();
-    controlButton.remove();
-}
+    secondsToStart = millis() / 1000,1; // "starts" a timer made more clear within showTimeToWin()
 
-function showControl() {
-    controlButton.scale = 3;
-    
-    controlButton.text = ("spam left & right arrow to increase score\nget to 50 score to start a fire");
-}
-function makeWinningEmberJelliesOfVictory() {
-    WinningEmberJellyOfVictory = new Sprite(width/2,height/2+25,10,'d');
+    function updateGameVisuals () {
+        campfire.visible = true;
+        stick.visible = true;
+        startButton.remove();
+        controlButton.remove();
+    }
+    updateGameVisuals
+    changeGameState();
 
-    WinningEmberJellyOfVictory.color = color(255,random(160,170),random(59,67));
-    WinningEmberJellyOfVictory.strokeWeight = 0;
-    WinningEmberJellyOfVictory.scale = random(1,2);
-    WinningEmberJellyOfVictory.life = random(60,100);
-    WinningEmberJellyOfVictory.drag.x = 0.001;
-    WinningEmberJellyOfVictory.vel.x += random(0.3,-0.3)
-    WinningEmberJellyOfVictory.vel.y -= random(0.01,0.3)
-    WinningEmberJellyOfVictory.opacity = 0.5;
-    WinningEmberJellyOfVictory.overlaps(allSprites);
 }
 
 function endGame() {
-    textSize(32);
-    if (gameEnded == false) {
-        seconds = millis() / 1000;
-        secondsToWin = round(seconds - secondsToStart,1);
-        gameEnded = true;         
+    function showTimeToWin() {
+        if (gameEnded == false) {
+            seconds = millis() / 1000;
+            secondsToWin = round(seconds - secondsToStart,1);
+            gameEnded = true;       
+            text(`took ${secondsToWin} to win\nctrl + r to restart`,width/2, 400);   
+            textSize(32);
+        }
     }
-    makeWinningEmberJelliesOfVictory();
-    text(`took ${secondsToWin} to win\nctrl + r to restart`,width/2, 400); 
+
+    function makeWinningEmberJelliesOfVictory() {
+        WinningEmberJellyOfVictory = new Sprite(width/2,height/2+25,10,'d');
+        WinningEmberJellyOfVictory.color = color(255,random(160,170),random(59,67));
+        WinningEmberJellyOfVictory.strokeWeight = 0;
+        WinningEmberJellyOfVictory.scale = random(1,2);
+        WinningEmberJellyOfVictory.life = random(60,100);
+        WinningEmberJellyOfVictory.drag.x = 0.001;
+        WinningEmberJellyOfVictory.vel.x += random(0.3,-0.3)
+        WinningEmberJellyOfVictory.vel.y -= random(0.01,0.3)
+        WinningEmberJellyOfVictory.opacity = 0.5;
+        WinningEmberJellyOfVictory.overlaps(allSprites);
+    }
+    for (i=0; i<1; i++) { //doubles up on embers.
+        makeWinningEmberJelliesOfVictory();
+    }
+    showTimeToWin();
 }
+
 function draw() {
     textSize(100);
     if (gameStarted == true) { 
         background(230,230,230);
+
         function updateStick() {
             left = Math.min(kb.pressing("left"),1);
             right = Math.min(kb.pressing("right"),1);
@@ -112,6 +139,10 @@ function draw() {
             startGame();
         }
         if (controlButton.mouse.pressed()) {
+            function showControl() {
+                controlButton.scale = 3;
+                controlButton.text = (controlText);
+            } //tested spamming this: no issues
             showControl()
         }
     } 
